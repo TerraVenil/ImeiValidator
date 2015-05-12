5529b1a3fb1d0a14726cf0262051d3c2af8547dd
Oleksii Borodenko <oborodenko@dio-soft.com>
2015-05-12 19:14:28 +0300

    Added NUnit tests project for business layer.

---
+++ ImeiValidator.NUnitTests/ImeiProviderTests.cs
@@ -0,0 +1,24 @@
+﻿using ImeiValidator.WebService.ImeiService;
+
+using Moq;
+
+using NUnit.Framework;
+
+namespace ImeiValidator.NUnitTests
+{
+    [TestFixture]
+    public class ImeiProviderTests
+    {
+        [Test]
+        public void TestGetImeiStatus()
+        {
+            var imeiService = new Mock<MicsServiceSoap>();
+            imeiService.Setup(x => x.GetItemStatusByImei("8965431")).Returns("Невідомий формат даних '8965431'");
+            var imeiProvider = new ImeiProvider(imeiService.Object);
+
+            var result = imeiProvider.GetImeiStatus("8965431");
+
+            Assert.That(result, Is.EqualTo("Невідомий формат даних '8965431'"));
+        }
+    }
+}
