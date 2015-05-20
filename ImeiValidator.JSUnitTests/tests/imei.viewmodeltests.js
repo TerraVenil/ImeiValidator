b35fff1f7b89b31fcb68c7c884f4a071059d7041
Oleksii Borodenko <oborodenko@dio-soft.com>
2015-05-20 12:06:34 +0300

    - remove obsolete awesome fonts; - add jstests on imei.viewmodel.js;

---
+++ ImeiValidator.JSUnitTests/tests/imei.viewmodeltests.js
@@ -0,0 +1,12 @@
+﻿(function ($, qUnit, imeiRequestViewModel) {
+    "use strict";
+    qUnit.test("initialize ImieRequestViewModel", function () {
+        var imeRequestViewModel = new imeiRequestViewModel();
+        qUnit.equal(imeRequestViewModel.number.length, 0);
+    });
+
+    qUnit.test("initialize ImieRequestViewModel with number", function () {
+        var imeRequestViewModel = new imeiRequestViewModel(6);
+        qUnit.equal(imeRequestViewModel.number(), 6);
+    });
+}(jQuery, QUnit, ImeiRequest));
